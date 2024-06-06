f98ad0457a18e98e24eedabc21ab6f308b95c64e
Viviane <arrghe.dev@gmail.com>
2024-06-06 14:32:21 +0200

    ajout d'un filtre pour filtrer les produits

--- components/Home.js
+++ components/Home.js
@@ -11,35 +11,62 @@ export default function Home() {
   // const { addToCart } = useCart();
   const dispatch = useDispatch();
   const [isCartVisible, setisCartVisible] = useState(false);
+  const [selectCategory, setSelectCategory] = useState(null);
 
   const toggleCatVisible = () => {
     setisCartVisible(!isCartVisible);
-  }
+  };
 
   const handleAddToCart = (product) => {
     dispatch(addItem(product));
-  }
+  };
+
+  const handleCategoryFilter = (category) => {
+    setSelectCategory(category);
+  };
+
+  const filterProducts = selectCategory
+    ? products.filter((product) => product.category === selectCategory)
+    : products;
 
   return (
     <div>
-      <header>
-        <h1 className="text-indigo-50 text-center my-[25px] text-3xl">
-          Page des desserts
-        </h1>
-      </header>
-      <main className="custom-ubuntu-font flex  justify-center items-center min-h-screen py-16">
-        <button
-          onClick={toggleCatVisible}
-          className="absolute top-0 right-0 mt-6 mr-5"
-        >
-          {isCartVisible ? "Masquer le panier" : "Afficher le panier"}
-        </button>
+      <header className="h-20 flex items-center ">
+        <div className="">
+          <h1 className="text-indigo-50 w-auto ml-10 my-[25px] text-3xl">
+            Page des desserts
+          </h1>
+          <div className="absolute top-0 right-80 mt-6 mr-5 ">
+            <select
+              onChange={(e) => handleCategoryFilter(e.target.value)}
+              value={selectCategory}
+              className="border border-stone-300 rounded px-2 py-3 w-80 mt-2"
+            >
+              <option value="">Toutes les catégories</option>
+              {Array.from(
+                new Set(products.map((product) => product.category))
+              ).map((category, index) => (
+                <option key={index} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button
+            onClick={toggleCatVisible}
+            className="absolute top-0 right-0 mt-6 mr-5"
+          >
+            {isCartVisible ? "Masquer le panier" : "Afficher le panier"}
+          </button>
           {isCartVisible && <ShoppingCart />}
+        </div>
+      </header>
+      <main className="custom-ubuntu-font flex flex-col justify-center items-center min-h-screen py-16">
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-          {products.map((product) => (
-            <DessertCard 
-              key={product.id} 
-              product={product} 
+          {filterProducts.map((product) => (
+            <DessertCard
+              key={product.id}
+              product={product}
               onAddToCart={() => handleAddToCart(product)}
             />
           ))}
