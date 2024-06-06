148f2d0a3ce774ba11410407e8faa86863257c0b
Viviane <arrghe.dev@gmail.com>
2024-06-06 14:34:41 +0200

    mise à jour du fichier index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 // pages/index.js
 import Home from "../components/Home";
-import ShoppingCart from "../components/ShoppingCart";
 
 export default function Index() {
   return (
     <div>
       <Home />
-      {/* <ShoppingCart /> */}
     </div>
   );
 }
